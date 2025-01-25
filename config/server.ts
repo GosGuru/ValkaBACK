358bb67b05977b72990c1511cade9f35464f6016
Maximo <maximogallo13@gmail.com>
2025-01-24 23:49:48 -0300

    Segundo commit - Subiendo proyecto Strapi

--- config/server.ts
+++ config/server.ts
@@ -1,10 +1,7 @@
-module.exports = ({ env }) => {
-  console.log("APP_KEYS:", env("APP_KEYS"));
-  return {
-    host: env("HOST", "0.0.0.0"),
-    port: env.int("PORT", 1337),
-    app: {
-      keys: env.array("APP_KEYS"),
-    },
-  };
-};
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+});
