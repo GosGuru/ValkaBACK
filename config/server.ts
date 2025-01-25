7f76fc7eccfa2dad17072abccde017edad14649b
Maximo <maximogallo13@gmail.com>
2025-01-24 23:41:33 -0300

    Primer commit - Subiendo proyecto Strapi

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,10 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-});
+module.exports = ({ env }) => {
+  console.log("APP_KEYS:", env("APP_KEYS"));
+  return {
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    app: {
+      keys: env.array("APP_KEYS"),
+    },
+  };
+};
